861acdaf738033d29c212a79506f1553295e9867
randaaz <randaaziz972@gmail.com>
2023-08-10 16:30:04 -0700

    Write and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Write a c program print without use  any functions listed in the NAME section of the man (3) printf or man (3) puts
+ * Description: Write a program print without usei any functios
  *
- * Return: Always 1 (not Scuccess)
+ * Return: 1 (not Scuccess)
 */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char ran[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,quote,59);
+	write(1, ran, 59);
 	return (1);
 }
