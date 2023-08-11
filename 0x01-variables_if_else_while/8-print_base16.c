66ea6e39955c51647bead343b6d88ea1108bfca3
randaaz <randaaziz972@gmail.com>
2023-08-11 10:44:48 -0700

    useing ACCI

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lowercase useing for
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char number;
+
+	for (number = 0; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+
+	for (number = 'a'; number <= 'f'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
