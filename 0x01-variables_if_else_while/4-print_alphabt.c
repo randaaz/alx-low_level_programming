156b48ce1034404ab6e595596bfbda6ab48f20d7
randaaz <randaaziz972@gmail.com>
2023-08-11 10:08:43 -0700

    useing while loop and if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,24 @@
-#include <stdio.h>
+# include <stdio.h>
 
 /**
  * main - Entry point
- * Description: using while loop and if founction
+ *
+ * Description: Prints the alphabet in lowercase, excluding 'q' and 'e'
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-        char alphabet = 'a';
+	char alphabet = 'a';
 
-        while (alphabet <= 'z')
-        {
-		if (alphabet != 'q' && alphabet != 'e' )
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
 		{
-                putchar(alphabet);
+			putchar(alphabet);
 		}
-                alphabet++;
-        }
-        putchar('\n');
-
-        return (0);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
 }
