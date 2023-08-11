0125c48f86cb95afff3ca487a45e9e9ff0b86dad
randaaz <randaaziz972@gmail.com>
2023-08-11 08:42:49 -0700

    using while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using while loop and if function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ran = 0;
+
+	while (ran <= 9)
+	{
+		printf("%d", ran);
+		ran++;
+	}
+	printf("\n");
+	return (0);
+}
