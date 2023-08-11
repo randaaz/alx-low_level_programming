b6922dc0d4f094e84339cdf00b31ffcfffbdd4d8
randaaz <randaaziz972@gmail.com>
2023-08-11 07:12:54 -0700

    useing while loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet
+ *
+ *  Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet = 'a';
+	char alphabet1 = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet1++;
+	}
+
+	while (alphabet1 <= 'Z')
+	{
+		putchar(alphabet1);
+		alphabet1++;
+	}
+	putchar('\n');
+	return (0);
+}
