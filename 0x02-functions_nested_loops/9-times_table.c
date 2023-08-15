c11ea8acf677675ef4fd30e4b1194cf89c0cc8d3
randaaz <randaaziz972@gmail.com>
2023-08-15 09:17:30 -0700

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,8 +30,10 @@ void times_table(void)
 			}
 			else
 			{
-				if (column != 0);
-				_putchar(' ');
+				if (column != 0)
+				{
+					_putchar(' ');
+				}
 				_putchar(result + '0');
 			}
 		}
