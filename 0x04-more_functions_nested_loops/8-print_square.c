f4d055f33888ce6f56e0453a3dc6eadcf18aa91a
randaaz <randaaziz972@gmail.com>
2023-08-17 04:30:00 -0700

    prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * print_square - draws a straight line in the terminal
+ * @size: The number of times the character '*' should be printed
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('*');
+		}
+		_putchar('\n');
+	}
+}
