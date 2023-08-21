f99282b1725b77e8c4d2409e908a70d0e4e92159
randaaz <randaaziz972@gmail.com>
2023-08-21 01:54:20 -0700

    prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: The string
+ *
+ * Return: void.
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '0');
+		str++;
+	}
+	_putchar('\n');
+}
