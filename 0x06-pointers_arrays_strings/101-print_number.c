8e4bd42655160f9b8bc2b5ba7505b61bdf02c0a3
randaaz <randaaziz972@gmail.com>
2023-08-23 09:50:17 -0700

    prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  print_number - prints an integer
+ *  @n : the number we want to print
+ *
+ *  Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
