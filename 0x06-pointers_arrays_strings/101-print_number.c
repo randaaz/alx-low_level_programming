5f3c0abc728390885d21f9a805203b0e730f76c5
randaaz <randaaziz972@gmail.com>
2023-08-23 10:08:55 -0700

    prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,16 @@
 
 void print_number(int n)
 {
-	unsigned int nu = n;
 
 	if (n < 0)
 	{
 
-		nu = -n;
+		n = -n;
 		_putchar('-');
 	}
-	if (nu / 10 != 0)
+	if (n / 10 != 0)
 	{
-		print_number(nu / 10);
+		print_number(n / 10);
 	}
-	_putchar(nu % 10 + '0');
+	_putchar(n % 10 + '0');
 }
