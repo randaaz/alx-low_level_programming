6cb1d981439c5684f34d65f95ef2123fd7d8542b
randaaz <randaaziz972@gmail.com>
2023-08-24 00:30:12 -0700

    adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,57 +10,44 @@
  * Return: The result of adding two numbers
  */
 
+#include "main.h"
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1 = 0;
-	int l2 = 0;
-	int cy = 0;
-	int sum, i, j, c;
+	int l1 = 0, l2 = 0, cy = 0, sum, i, j, c = 0;
 	char tmp;
 
-	while (n1[l1] != '\0')
-	{
+	while (n1[l1])
 		l1++;
-	}
-	while (n2[l2] != '\0')
-	{
+	while (n2[l2])
 		l2++;
-	}
-	if (l1 >  size_r - 1 || l2 > size_r - 1)
-	{
-		return (0);
-	}
+
+	if (l1 > size_r - 1 || l2 > size_r - 1)
+		return (NULL);
+
 	i = l1 - 1;
 	j = l2 - 1;
-	c = 0;
 
 	while (i >= 0 || j >= 0 || cy)
 	{
-		sum = cy;
-		if (i >= 0)
-		{
-			sum += n1[i] - '0';
-		}
-		if (j >= 0)
-		{
-			sum += n2[j] - '0';
-		}
+		sum = cy + (i >= 0 ? n1[i] - '0' : 0) + (j >= 0 ? n2[j] - '0' : 0);
 		cy = sum / 10;
-		r[c] = sum % 10 + '0';
+		r[c++] = sum % 10 + '0';
 		i--;
 		j--;
-		c++;
 	}
+
 	if (c >= size_r)
-	{
-		return (0);
-	}
+		return (NULL);
+
 	r[c] = '\0';
+
 	for (i = 0, j = c - 1; i < j; i++, j--)
 	{
 		tmp = r[i];
 		r[i] = r[j];
 		r[j] = tmp;
 	}
+
 	return (r);
 }
