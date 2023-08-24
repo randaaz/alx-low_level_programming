c2ba218aa400cd728ba716b8958bd3ddb67ca4f2
randaaz <randaaziz972@gmail.com>
2023-08-24 03:13:53 -0700

    last task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,39 +1,90 @@
 #include "main.h"
 
 /**
- * print_buffer -  prints a buffer
+ * ACCII - Checks if an ASCII character is printable
+ * @a: The ASCII character code to check
+ *
+ * Return: 1 if the character is printable, 0 otherwise
+ */
+
+int ACCII(int a)
+{
+	return (a >= 32 && a <= 126);
+}
+
+/**
+ * hexa - Prints hexadecimal representation of buffer
+ * @b: The buffer
+ * @st: Start index
+ * @en: End index
+ *
+ * Return: void
+ */
+
+void hexa(char *b, int st, int en)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < en)
+			printf("%02x", *(b + st + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+
+}
+
+/**
+ * p_ACCII - Prints printable ASCII representation of buffer
+ * @b: The buffer
+ * @st: Start index
+ * @en: End index
+ *
+ * Return: void
+ */
+
+void p_ACCII(char *b, int st, int en)
+{
+	int c, i = 0;
+
+	while (i < en)
+	{
+		c = *(b + i + st);
+		if (!ACCII(c))
+		c = 46;
+		printf("%c", c);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - Prints a buffer with hexadecimal and printable ASCII
  * @b: The buffer
  * @size: The number of bytes to print
  *
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	for (int i = 0; i < size; i += 10)
+	int st, en;
+
+	if (size > 0)
 	{
-		printf("%08x: ", i);
-
-		for (int j = 0; j < 10; j++)
+		for (st = 0; st < size; st += 10)
 		{
-			if (i + j < size)
-				printf("%02x%s", (unsigned char)b[i + j], j % 2 == 1 ? " " : "");
-			else
-				printf("   ");
+			en = (size - st < 10) ? size - st : 10;
+			printf("%08x: ", st);
+			hexa(b, st, en);
+			p_ACCII(b, st, en);
+			printf("\n");
+
 		}
-
-		printf(" ");
-
-		for (int j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-				printf("%c", b[i + j] >= 32 && b[i + j] <= 126 ? b[i + j] : '.');
-			else
-				printf(" ");
-		}
-
-		printf("\n");
 	}
-
-	if (size <= 0)
+	else
 		printf("\n");
 }
