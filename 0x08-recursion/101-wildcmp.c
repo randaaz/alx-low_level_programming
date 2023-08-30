c6b2f0dc0540cf05c2e98f9e48a6cd18287d857a
randaaz <randaaziz972@gmail.com>
2023-08-30 09:53:58 -0700

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,6 +7,7 @@
  *
  * Return: 1 if strings can be considered identical, 0 otherwise
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
@@ -16,6 +17,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
+	}
 
 	if (*s2 == '*')
 	{
