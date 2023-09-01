fe2fd7e45226572aa23b8abaeb5e20b91cc8a74a
randaaz <randaaziz972@gmail.com>
2023-09-01 03:21:33 -0700

    adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,39 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * is_digit - Checks if a character is a digit (0-9).
+ * @c: The character to be checked.
  *
- * Description: This program will adds positive numbers.
- * the result of the multiplication, followed by a new line
- * @argc: the number of argument
- * @argv: the array of argument
+ * This function determines whether the given character is a digit or not.
+ * It returns 1 if the character is
+ * a digit (between '0' and '9'), and 0 otherwise.
  *
- * Return: 0 if it is Success or 1 does not receive two arguments
+ * Return: 1 if @c is a digit, 0 otherwise.
  */
 int main(int argc, char *argv[])
 {
-	int i;
+        int i;
 
-	int totalSum = 0;
-	int *j;
+        int totalSum = 0;
+        char *j;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = argv[argc]; *j; j++)
-		{
-			if (*j < '0' || *j > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
+        for (i = 1; i < argc; i++)
+        {
+                for (j = argv[argc]; *j; j++)
+                {
+                        if (*j < '0' || *j > '9')
+                        {
+                                printf("Error\n");
+                                return (1);
+                        }
+                }
 
-		totalSum += atoi(argv[argc]);
-	}
+                totalSum += atoi(argv[argc]);
+        }
 
-	printf("%d\n", totalSum);
+        printf("%d\n", totalSum);
 
-	return (0);
+        return (0);
 }
