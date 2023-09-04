a1507e9721cdef387c453a9fc12b19e8d5d67278
randaaz <randaaziz972@gmail.com>
2023-09-04 02:59:34 -0700

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,19 +15,23 @@ char *str_concat(char *s1, char *s2)
 	char *new;
 	int l1 = 0;
 	int l2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			l1++;
-		}
-		for (i = 0; s2[i] != '\0'; i++)
-		{
-			l2++;
-		}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		l1++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		l2++;
+	}
 	new = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
 
 	if (new == NULL)
@@ -44,7 +48,7 @@ char *str_concat(char *s1, char *s2)
 		{
 			new[i + j] = s2[j];
 		}
-		 new[i + j] = '\0';
+		new[i + j] = '\0';
 	}
 	return (new);
 }
