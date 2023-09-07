0a40089393b7c44210a9c02a154deaa518d48c7a
randaaz <randaaziz972@gmail.com>
2023-09-07 05:53:31 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_error - print Error, followed by a new line
- */
-void print_error()
-{
-	char *error_message = "Error\n";
-	while (*error_message)
-	{
-		_putchar(*error_message);
-		error_message++;
-	}
-}
-
-/**
- * print_number - print numbers
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
-}
-
-/**
  * is_digit - Check if a string contains only digits
+ * @str: The input string
  * Return: 1 if all characters are digits, 0 otherwise
  */
 int is_digit(char *str)
@@ -40,9 +12,7 @@ int is_digit(char *str)
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
-		{
 			return 0;
-		}
 		str++;
 	}
 	return 1;
@@ -50,31 +20,30 @@ int is_digit(char *str)
 
 /**
  * main - Entry point
- * Description: multiplies two positive numbers
- * Return: the result, followed by a new line
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
 	if (argc != 3)
 	{
-		print_error();
-		exit(98);
+		printf("Error\n");
+		return (98);
 	}
+
 	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
-		print_error();
-		exit(98);
+		printf("Error\n");
+		return (98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	if (mul == 0)
-	{
-		_putchar('0');
-	}
-	print_number(mul);
-	_putchar('\n');
-	return 0;
+
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	// Implement multiplication logic here and store the result in a variable.
+
+	printf("%s\n", result); // Print the result
+	return (0);
 }
 
