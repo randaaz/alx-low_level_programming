c08c6054bf5c2de70c8a9ad823742f8532377c28
randaaz <randaaziz972@gmail.com>
2023-09-07 09:35:48 -0700

    program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,31 +3,101 @@
 #include <string.h>
 
 /**
- * _putchar - Write a character to stdout
+ * _putchar - Write a character to stdout.
  * @c: The character to print
  * Return: 1 on success, -1 on error
  */
+
+int _putchar(char c);
+
+/**
+ * is_valid_digit - Check if a character is a valid digit.
+ * @c: The character to check
+ * Return: 1 if valid, 0 if not
+ */
+
+int is_valid_digit(char c);
+
+/**
+ * multiply - Multiply two positive numbers.
+ * @num1: The first number as a string.
+ * @num2: The second number as a string.
+ */
+
+void multiply(char *num1, char *num2);
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 98 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	for (i = 0; num1[i] != '\0'; i++)
+	{
+		if (!is_valid_digit(num1[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	for (i = 0; num2[i] != '\0'; i++)
+	{
+		if (!is_valid_digit(num2[i]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+	}
+
+	multiply(num1, num2);
+
+	return (0);
+}
+
+/**
+ * _putchar - Write a character to stdout.
+ * @c: The character to print
+ * Return: 1 on success, -1 on error
+ */
+
 int _putchar(char c)
 {
 	return (putchar(c));
 }
 
-
 /**
- * is_valid_digit - Check if a character is a valid digit
+ * is_valid_digit - Check if a character is a valid digit.
  * @c: The character to check
  * Return: 1 if valid, 0 if not
  */
+
 int is_valid_digit(char c)
 {
 	return (c >= '0' && c <= '9');
 }
 
 /**
- * multiply - Multiply two positive numbers
- * @num1: The first number as a string
- * @num2: The second number as a string
+ * multiply - Multiply two positive numbers.
+ * @num1: The first number as a string.
+ * @num2: The second number as a string.
  */
+
 void multiply(char *num1, char *num2)
 {
 	int len1 = strlen(num1);
@@ -69,47 +139,4 @@ void multiply(char *num1, char *num2)
 	_putchar('\n');
 
 	free(result);
-}
-/**
- * main - Entry point
- * Description: Multiplies two positive numbers
- * @argc: The number of arguments
- * @argv: The array of arguments
- * Return: 0 on success, 98 on failure
- */
-int main(int argc, char *argv[])
-{
-	char *num1, *num2;
-	int i;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	for (i = 0; num1[i] != '\0'; i++)
-	{
-		if (!is_valid_digit(num1[i]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
-
-	for (i = 0; num2[i] != '\0'; i++)
-	{
-		if (!is_valid_digit(num2[i]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
-
-	multiply(num1, num2);
-
-	return (0);
 }
