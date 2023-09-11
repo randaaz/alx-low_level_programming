1175fb22e2115af8bec4ba406bd711f15dec4345
randaaz <randaaziz972@gmail.com>
2023-09-11 00:05:53 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+contain
+a new type struct dog
+a function that initialize a variable of type struct dog
+a function that prints a struct dog
+a new type dogk_t as a new name for the type struct dog
+a function that creates a new dog
+Write a function that frees dogs
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -  Describes a dog with its name, age, and owner
+ *@name: The name of the dog
+ *@age: The age of the dog
+ *@owner: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
