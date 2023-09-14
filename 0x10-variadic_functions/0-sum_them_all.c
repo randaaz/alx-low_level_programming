cf14c2ffc3275e9ca9c62a3e3f651a0c30cf2b87
randaaz <randaaziz972@gmail.com>
2023-09-14 03:07:42 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i = n;
 	va_list args;
+	int sum = 0, i = n;
 
 	if (!n)
 		return (0);
@@ -18,10 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	while (i--)
-	{
 		sum += va_arg(args, int);
-	}
-
 	va_end(args);
 
 	return (sum);
