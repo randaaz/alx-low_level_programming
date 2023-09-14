3cc67da458322bf778407a4ad43b21368db30e43
randaaz <randaaziz972@gmail.com>
2023-09-14 03:20:29 -0700

    fix error

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,15 +4,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list args);
-void print_integer(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
 
 /**
  * struct variable - Struct variable
