c2758c7754a9c1e90acf0a41a1e388396fcdeafb
randaaz <randaaziz972@gmail.com>
2023-09-14 02:23:45 -0700

    prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 		printf("%s", str);
-		if (i < n - 1 && separator != NULL)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
