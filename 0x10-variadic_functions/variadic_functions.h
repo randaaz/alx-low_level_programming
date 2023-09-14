86898ff56f089546c78cefe92607418bffd3e372
randaaz <randaaziz972@gmail.com>
2023-09-14 03:17:37 -0700

    fix error

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,6 +14,15 @@ void print_integer(va_list args);
 void print_float(va_list args);
 void print_string(va_list args);
 
+/**
+ * struct variable - Struct variable
+ * @variable: The variable character
+ * @function: The function pointer taking a va_list argument
+ *
+ * Description: This struct defines a variable character and a function pointer
+ *              to a function that takes a va_list argument.
+ */
+
 struct variable
 {
     char variable;
