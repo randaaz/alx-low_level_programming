5a33955e5cd4b8562e1c8664b7459f45aa7c0c62
randaaz <randaaziz972@gmail.com>
2023-09-14 03:00:56 -0700

    fix error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
@@ -10,21 +9,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum = 0, i = n;
-     va_list args;
+	int sum = 0, i = n;
+	va_list args;
 
-    if (!n)
-        return (0);
+	if (!n)
+		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
 
-    while (i--)
-    {
-        sum += va_arg(args, int);
-    }
+	while (i--)
+	{
+		sum += va_arg(args, int);
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (sum);
+	return (sum);
 }
 
