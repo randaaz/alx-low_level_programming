9c1eecd61a522a4262fd442b1e720b6d9629288e
randaaz <randaaziz972@gmail.com>
2023-09-25 09:57:12 -0700

    fix error

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,63 @@
 #include "lists.h"
 
 /**
- * free_listint_safe -  frees a listint_t list
- * @h: Pointer to a pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A pointer to a pointer to the head of the list.
  *
- * Return: the size of the list that was free’d
+ * Return: The number of nodes that were freed.
  */
-
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *last = *h, *temp;
-	size_t i = 0;
+	size_t i;
+	listint_t *j, *ln;
+	int l = 1;
 
-	while (last != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+	ln = hit(*h);
+	for (i = 0; (*h != ln || l) && *h != NULL; *h = j)
 	{
-		temp = last->next;
-		free(last);
 		i++;
-		if (last <= temp)
+		j = (*h)->next;
+		if (*h == ln && l)
 		{
-			*h = NULL;
-			break;
+			if (ln == ln->next)
+			{
+				free(*h);
+				break;
+			}
+			i++;
+			j = j->next;
+			free((*h)->next);
+			l = 0;
 		}
-		last = temp;
+		free(*h);
 	}
-
 	*h = NULL;
 	return (i);
+}
+
+/**
+ * hit - Finds the node at which a linked list has a cycle.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: The node where the cycle begins, or NULL if there is no cycle.
+ */
+
+listint_t *hit(listint_t *head)
+{
+	listint_t *new, *last;
+
+	if (head == NULL)
+		return (NULL);
+	for (last = head->next; last != NULL; last = last->next)
+	{
+		if (last == last->next)
+			return (last);
+		for (new = head; new != last; new = new->next)
+			if (new == last->next)
+				return (last->next);
+	}
+	return (NULL);
 }
