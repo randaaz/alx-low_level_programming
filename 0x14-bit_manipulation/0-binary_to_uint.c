15ab7ff971246e3c019b7a7a31bf403855edfbb6
randaaz <randaaziz972@gmail.com>
2023-09-28 04:06:48 -0700

    fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,14 +12,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int n = 0;
 
 	if (!b)
+	{
 		return (0);
-	while (*b != NULL)
+	}
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
-		n = n * 2 + (*b - '0');
+		n = (n * 2) + (*b - '0');
 		b++;
 	}
 	return (n);
