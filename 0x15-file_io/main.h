3ec4d30d37c4722e0227da3ea10391a7b12704ff
randaaz <randaaziz972@gmail.com>
2023-10-02 05:17:05 -0700

    fix error

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define _SIZE 1024
+
 #include<stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,10 +13,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _len(char *c);
-void print_m(ELF64_Ehde d);
-void print_c(ELF64_Ehdr d);
-void print_o(ELF64_Ehdr d);
-void print_d(ELF64_Ehdr d);
-void print_v(ELF64_Ehdr d);
-
 #endif
