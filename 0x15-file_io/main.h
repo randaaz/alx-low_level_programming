6cdfad71369d319bcec66a417ef5edd46354e227
randaaz <randaaziz972@gmail.com>
2023-10-01 21:34:48 -0700

    modify

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _len(char *c);
 void print_m(ELF64_Ehde d);
 void print_c(ELF64_Ehdr d);
 void print_o(ELF64_Ehdr d);
