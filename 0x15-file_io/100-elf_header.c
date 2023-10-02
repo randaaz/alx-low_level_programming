079975e4ba33e1ec2d6e5b9c549049805669dfc5
randaaz <randaaziz972@gmail.com>
2023-10-02 07:12:35 -0700

    fix the error

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,30 +1,71 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 #include <elf.h>
 
+void print_m(Elf64_Ehdr d);
+void print_c(Elf64_Ehdr d);
+void print_d(Elf64_Ehdr d);
+void print_v(Elf64_Ehdr d);
+void print_o(Elf64_Ehdr d);
+void print_a(Elf64_Ehdr d);
+void print_t(Elf64_Ehdr d);
+void print_entry(Elf64_Ehdr d);
 
 /**
- * print_m - Print the ELF header's magic bytes.
- * @d: ELF header structure
+ * main - Entry point
+ * @argc: number
+ * @argv: An array
+ *
+ * Return: 0 on successful execution
  */
 
-void print_m(Elf64_Ehdr d)
+int main(int argc, char **argv)
 {
-	int j;
+	int num;
+	Elf64_Ehdr d;
+	ssize_t b;
 
-	printf("  Magic:   ");
-	for (j = 0; j < EI_NIDENT; j++)
-	{
-		printf("%2.2x%s", d.e_ident[j], j == EI_NIDENT - 1 ? "\n" : " ");
-	}
+	if (argc != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	num = open(argv[1], O_RDONLY);
+	if (num == -1)
+		dprintf(STDERR_FILENO, "Can't open file: %s\n", argv[1]), exit(98);
+	b = read(num, &d, sizeof(d));
+	if (b < 1 || b != sizeof(d))
+		dprintf(STDERR_FILENO, "Can't read from file: %s\n", argv[1]), exit(98);
+	if (d.e_ident[EI_MAG0] != ELFMAG0 || d.e_ident[EI_MAG1] != ELFMAG1 ||
+			d.e_ident[EI_MAG2] != ELFMAG2 || d.e_ident[EI_MAG3] != ELFMAG3)
+		dprintf(STDERR_FILENO, " NOT ELF file: %s\n", argv[1]), exit(98);
+	printf("ELF Header:\n");
+	print_m(d);
+	print_c(d);
+	print_d(d);
+	print_v(d);
+	print_o(d);
+	print_a(d);
+	print_t(d);
+	print_entry(d);
+	if (close(num))
+		dprintf(STDERR_FILENO, " Error closing file: %d\n", num), exit(98);
+	return (EXIT_SUCCESS);
 }
 
 /**
- * print_c - Print the ELF header's class information.
- * @d: ELF header structure
+ * print_m - Print the magic
+ * @d: The ELF
  */
+void print_m(Elf64_Ehdr d)
+{
+	int i;
 
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%2.2x%s", d.e_ident[i], i == EI_NIDENT - 1 ? "\n" : " ");
+}
+
+/**
+ * print_c - Print the ELF class
+ * @d: The ELF
+ */
 void print_c(Elf64_Ehdr d)
 {
 	printf("  Class:                             ");
@@ -44,81 +85,9 @@ void print_c(Elf64_Ehdr d)
 }
 
 /**
- * print_o - Print the ELF header's OS/ABI information.
- * @d: ELF header structure
+ * print_d - Print the endianness
+ * @d: The ELF
  */
-
-void print_o(Elf64_Ehdr d)
-{
-	printf("  OS/ABI:                            ");
-	switch (d.e_ident[EI_OSABI])
-	{
-		case ELFOSABI_NONE:
-			printf("UNIX - System V");
-			break;
-		case ELFOSABI_HPUX:
-			printf("UNIX - HP_UX");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD");
-			break;
-		case ELFOSABI_LINUX:
-			printf("UNIX - Linux");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris");
-			break;
-		case ELFOSABI_AIX:
-			printf("UNIX - AIX");
-			break;
-		case ELFOSABI_IRIX:
-			printf("UNIX - IRIX");
-			break;
-		case ELFOSABI_FREEBSD:
-			printf("UNIX - FreeBSD");
-			break;
-		case ELFOSABI_TRU64:
-			printf("UNIX - TRU64");
-			break;
-		default:
-			printf("<unknown: %x>", d.e_ident[EI_OSABI]);
-			break;
-	}
-	printf("\n");
-}
-
-/**
- * print_p_o_m - Print the ELF header's processor-specific OS/ABI information.
- * @d: ELF header structure
- */
-
-void print_p_o_m(Elf64_Ehdr d)
-{
-	switch (d.e_ident[EI_OSABI])
-	{
-		case ELFOSABI_MODESTO:
-			printf("Non-ELF - Modesto");
-			break;
-		case ELFOSABI_OPENBSD:
-			printf("UNIX - OpenBSD");
-			break;
-		case ELFOSABI_STANDALONE:
-			printf("Standalone APP");
-			break;
-		case ELFOSABI_ARM:
-			printf("ARM");
-			break;
-		default:
-			printf("<unknown: %x>", d.e_ident[EI_OSABI]);
-			break;
-	}
-}
-
-/**
- * print_d - Print the ELF header's data format information.
- * @d: ELF header structure
- */
-
 void print_d(Elf64_Ehdr d)
 {
 	printf("  Data:                              ");
@@ -138,112 +107,124 @@ void print_d(Elf64_Ehdr d)
 }
 
 /**
- * print_v - Print the ELF header's version information.
- * @d: ELF header structure
+ * print_v - Print ELF version
+ * @d: The ELF
  */
-
 void print_v(Elf64_Ehdr d)
 {
 	printf("  Version:                           %d", d.e_ident[EI_VERSION]);
 	switch (d.e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
-			printf(" (current)");
+			printf(" (current)\n");
 			break;
 		default:
-			printf("%s", "");
+			printf("\n");
+	}
+}
+
+/**
+ * print_o - Print the Operating System/ABI
+ * @d: The ELF
+ */
+void print_o(Elf64_Ehdr d)
+{
+	printf("  OS/ABI:                            ");
+	switch (d.e_ident[EI_OSABI])
+	{
+		case ELFOSABI_NONE:
+			printf("UNIX - System V");
 			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - Linux");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris");
+			break;
+		case ELFOSABI_AIX:
+			printf("UNIX - AIX");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD");
+			break;
+		case ELFOSABI_OPENBSD:
+			printf("UNIX - OpenBSD");
+			break;
+		case ELFOSABI_STANDALONE:
+			printf("Standalone App");
+			break;
+		case ELFOSABI_ARM:
+			printf("ARM");
+			break;
+		default:
+			print_a(d);
 	}
 	printf("\n");
 }
 
 /**
- * print_a - Print the ELF header's ABI version information.
- * @d: ELF header structure
+ * print_a - Print more detailed OS/ABI information
+ * @d: The ELF
  */
-
 void print_a(Elf64_Ehdr d)
+{
+	switch (d.e_ident[EI_OSABI])
+	{
+		case ELFOSABI_MODESTO:
+			printf("Nove11 - Modesto");
+			break;
+		default:
+			printf("<unknown: %x>", d.e_ident[EI_OSABI]);
+	}
+}
+
+/**
+ * print_t - Print the ABI version
+ * @d: The ELF
+ */
+void print_t(Elf64_Ehdr d)
 {
 	printf("  ABI Version:                       %d\n", d.e_ident[EI_ABIVERSION]);
 }
 
 /**
- * print_t - Print the ELF header's type information.
- * @d: ELF header structure
+ * print_entry - Print the ELF type
+ * @d: The ELF
  */
-
-void print_t(Elf64_Ehdr d)
+void print_entry(Elf64_Ehdr d)
 {
-	char *ptr = (char *)&d.e_type;
-	int j = 0;
+	int i, l = 0;
+	unsigned char *p = (unsigned char *)&d.e_entry;
 
-	printf("  Type:                              ");
-	if (d.e_ident[EI_DATA] == ELFDATA2MSB)
-		j = 1;
-	switch (ptr[j])
+	printf("  Entry point address:               0x");
+	if (d.e_ident[EI_DATA] != ELFDATA2MSB)
 	{
-		case ET_NONE:
-			printf("NONE (None)");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)");
-			break;
-		default:
-			printf("<unknown>: %x", ptr[j]);
-			break;
+		i = (d.e_ident[EI_CLASS] == ELFCLASS64) ? 7 : 3;
+		while (!p[i])
+			i--;
+		printf("%x", p[i--]);
+		for (; i >= 0; i--)
+			printf("%02x", p[i]);
+		printf("\n");
 	}
-	printf("\n");
-}
-
-/**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
- *
- * Return: 0 on success, 98 on error
- */
-
-int main(int argc, char **argv)
-{
-	int ptr;
-	Elf64_Ehdr d;
-	ssize_t n;
-
-	if (argc != 2)
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-	return (98);
-	ptr = open(argv[1], O_RDONLY);
-	if (ptr == -1)
-		dprintf(STDERR_FILENO, "Can't open file : %s\n", argv[1]);
-	return (98);
-	n = read(ptr, &d, sizeof(d));
-	if (n < 1 || n != sizeof(d))
-		dprintf(STDERR_FILENO, "Can't read file : %s\n", argv[1]);
-	return (98);
-	if (d.e_ident[0] == 0x7f && d.e_ident[1] == 'E' &&
-			d.e_ident[2] == 'L' && d.e_ident[3] == 'F')
-		printf("ELF Header:\n");
 	else
-		dprintf(STDERR_FILENO, "NOT ELF file: %s\n", argv[1]);
-	return (98);
-	print_m(d);
-	print_c(d);
-	print_d(d);
-	print_v(d);
-	print_o(d);
-	print_a(d);
-	print_t(d);
-	if (close(ptr) == -1)
-		dprintf(STDERR_FILENO, "ERROR close file descriptor: %d\n", ptr);
-	return (98);
-	return (0);
+	{
+		i = 0;
+		l = (d.e_ident[EI_CLASS] == ELFCLASS64) ? 7 : 3;
+		while (!p[i])
+			i++;
+		printf("%x", p[i++]);
+		for (; i <= l; i++)
+			printf("%02x", p[i]);
+		printf("\n");
+	}
 }
