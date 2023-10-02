bdd90c9625a3f1a75bcd65c28f1d3e62b3610e4a
randaaz <randaaziz972@gmail.com>
2023-10-02 05:18:46 -0700

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	int ptr;
 	ssize_t b = 0, l = _len(text_content);
 
-	if (!filename || !text_content)
+	if (!filename)
 	{
 		return (-1);
 	}
