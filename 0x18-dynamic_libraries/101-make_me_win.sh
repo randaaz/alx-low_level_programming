4ce2444c437d58601f7a179f162901d088535967
randaaz <randaaziz972@gmail.com>
2023-11-13 01:05:43 -0800

    change p to P

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-wget -p https://github.com/randaaz/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
+wget -P https://github.com/randaaz/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
